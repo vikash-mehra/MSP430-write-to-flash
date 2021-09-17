133725a039bdc9694cea0c85e821a55bf9af6e06
vikash-mehra <88857899+vikash-mehra@users.noreply.github.com>
2021-09-17 10:43:44 -0700

    Create writeToFlash.c

---
+++ writeToFlash.c
@@ -0,0 +1,18 @@
+#include <msp430.h>
+#include <stdint.h>
+
+#include "writeToFlash.h"
+
+void write_flash(char data, int *addr){ //Writes data in address
+    SYSBSLC &= ~SYSBSLPE;               // Unprotect BSL
+    while(BUSY & FCTL3);                //wait for flash memory to be ready
+    FCTL1 = 0xA500 | WRT;               //enable write
+    FCTL3 = 0xA500 | LOCKA;             //locks segment A, which contain important information
+    while(BUSY & FCTL3);                //wait for flash memory to be ready
+    *addr = data;                       //write data in addr
+    addr++;                             //increment pointer (which isnt really necessary)
+    while(BUSY & FCTL3);                //wait for flash memory to be ready
+    FCTL1 = 0xA500;                     //lock writing
+    FCTL3 = 0xA500 | LOCK;
+    SYSBSLC = SYSBSLPE + SYSBSLSIZE0 + SYSBSLSIZE1;  // Protect the BSL
+}
